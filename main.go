f41ab5407d872e3ea608869be2c6d2491ff32032
Hiroaki Suzuki <rirazou05410@gmail.com>
2021-02-06 22:11:03 +0900

    WIP: refactoring

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"git-release-materials/argument"
 	"io/ioutil"
 	"log"
@@ -12,28 +12,45 @@ import (
 )
 
 func main() {
+	args := getArgs()
+
+	changeDirectory(args)
+	verifyManagedByGit(args)
+
+	switch args.Command {
+	case "list":
+		list := getList(args)
+
+		outputDir := "grm_" + time.Now().Format("20060102_030405")
+		outputDirPath := filepath.Join(args.OutputDir, outputDir)
+		err := os.MkdirAll(outputDirPath, 0744)
+		if err = ioutil.WriteFile(filepath.Join(outputDirPath, "list.txt"), []byte(list), 0644); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal(errors.New("the specified subcommand is not supported. " + args.Command))
+	}
+}
+
+func getArgs() argument.Args {
 	args, err := argument.GetArgs()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = os.Chdir(args.WorkDir)
+	return args
+}
+
+func changeDirectory(args argument.Args) {
+	err := os.Chdir(args.WorkDir)
 	if err != nil {
-		log.Fatal()
-	}
-
-	if _, err := os.Stat(".git"); os.IsNotExist(err) {
-		log.Fatal("This directory is not managed by Git. ", args.WorkDir)
-	}
-
-	list := getList(args)
-	fmt.Println(list)
-
-	outputDir := "grm_" + time.Now().Format("20060102_030405")
-	outputDirPath := filepath.Join(args.OutputDir, outputDir)
-	err = os.MkdirAll(outputDirPath, 0744)
-	if err = ioutil.WriteFile(filepath.Join(outputDirPath, "list.txt"), []byte(list), 0644); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func verifyManagedByGit(args argument.Args) {
+	if _, err := os.Stat(".git"); os.IsNotExist(err) {
+		log.Fatal("the specified directory is not under git control. ", args.WorkDir)
 	}
 }
 
